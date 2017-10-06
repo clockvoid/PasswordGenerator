153161d9a71df55cfc7bdc8624e8258af174c75f
clockvoid <hidenari_1@yahoo.co.jp>
2017-10-06 23:12:00 +0900

    add: selecting type of char mode

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = putStrLn =<< generateAStringByRandomCharacters 16
+main = putStrLn =<< password ' ' 16
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,32 @@
 module Lib
-    ( generateAStringByRandomCharacters
-    ) where
+  ( password
+  ) where
 
 import System.Random
+import Control.Monad (replicateM)
+
+numbers :: String
+numbers = "numbers"
+
+randAll :: IO Char
+randAll = randomRIO ('0', 'z') :: IO Char
 
 randAlpha :: IO Char
-randAlpha = getStdRandom $ randomR ('0', 'z')
+randAlpha = do
+  a <- randomRIO (0, 1) :: IO Int
+  [randomRIO ('a', 'z') :: IO Char, randomRIO ('A', 'Z') :: IO Char] !! a
 
-generateAStringByRandomCharacters :: Int -> IO [Char]
-generateAStringByRandomCharacters 0 = return ""
-generateAStringByRandomCharacters n = 
-    randAlpha >>= \c ->
-    generateAStringByRandomCharacters (n - 1) >>= \b ->
-    return $ [c] ++ b
+randNum :: IO Char
+randNum = randomRIO ('0', '9') :: IO Char
+
+randSymbol :: IO Char
+randSymbol = randomRIO ('!', '@') :: IO Char
+
+password :: Char -> Int -> IO [Char]
+password m n = replicateM n rc
+  where
+  rc = case m of
+    'a' -> randAlpha
+    'n' -> randNum
+    's' -> randSymbol
+    _ -> randAll
