d230fae29b0c178855afd89dffd41a2d3adcea95
clockvoid <hidenari.t1997nov@gmail.com>
2018-05-23 11:49:30 +0900

    add: files functions

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,15 @@ module Main where
 
 import Password
 import System.Environment
+import Files
 
 main :: IO ()
 main = do
+  let file = createFile "passwordFile"
   args <- getArgs
-  putStrLn =<< password (case args of
-                                  ["alpha"] -> 'a'
-                                  ["num"] -> 'n'
-                                  ["symbol"] -> 's'
-                                  _ -> ' ')
-                        16
+  pass <-  password (case args of
+    ["alpha"] -> 'a'
+    ["num"] -> 'n'
+    ["symbol"] -> 's'
+    _ -> ' ') 16
+  saveFile file pass
--- src/Files.hs
+++ src/Files.hs
@@ -1,10 +1,39 @@
+{- |
+Module : File
+Description : amodule for management files
+
+This module is to manage files to save Passwords etc.
+We want to manage file names and file contents, we make a State Monad for do it.:w
+ -}
+
 module Files
   (
+    saveFile
+    , createFile
+    , setFilePath
+    , getFilePath
   ) where
 
+import Control.Monad.Trans.State.Lazy
 import Password
 
+-- encryption for Passwords
 encrypt :: String -> String
 encrypt str = str
 
+type File = State FilePath FilePath
 
+saveFile :: File -> String -> IO ()
+saveFile file = writeFile $ getFilePath file
+
+setFilePath :: String -> File
+setFilePath name = do
+  v <- get
+  put name
+  return v
+
+getFilePath :: File -> String
+getFilePath file = execState file "something"
+
+createFile :: String -> File
+createFile = setFilePath
